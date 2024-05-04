5b24bbd0a2562533b3b47fdf325649a677e01150
Cyrine <cyrine.karoui123@gmail.com>
2024-05-04 18:34:44 +0100

    photo login

--- src/main/java/com/example/greetingmvc/web/controllers/AuthController.java
+++ src/main/java/com/example/greetingmvc/web/controllers/AuthController.java
@@ -23,5 +23,27 @@ public class AuthController {
         model.addAttribute("user", user);
         return "register";
     }
+
+    @PostMapping("/register/save")
+    public String registration(@Valid @ModelAttribute("user") UserDto userDto,
+                               BindingResult result,
+                               Model model){
+        User existingUser = userService.findUserByEmail(userDto.getEmail());
+
+        if(existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()){
+            result.rejectValue("email", null,
+                    "There is already an account registered with the same email");
+        }
+
+        if(result.hasErrors()){
+            model.addAttribute("user", userDto);
+            return "/register";
+        }
+
+        userService.saveUser(userDto);
+        return "redirect:/register?success";
+    }
+}
+
 }
 
