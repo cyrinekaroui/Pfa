b531e1e4a4c51a805fb46033933f1625083b97be
Cyrine <cyrine.karoui123@gmail.com>
2024-05-04 19:19:27 +0100

    registration done

--- src/main/java/com/example/greetingmvc/web/controllers/AuthController.java
+++ src/main/java/com/example/greetingmvc/web/controllers/AuthController.java
@@ -1,10 +1,16 @@
 package com.example.greetingmvc.web.controllers;
-
+import com.example.greetingmvc.web.service.UserService;
+import com.example.greetingmvc.web.models.User;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.greetingmvc.web.models.Dto.UserDto;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class AuthController {
 
@@ -26,8 +32,7 @@ public class AuthController {
     public String registration(@Valid @ModelAttribute("user") UserDto userDto,
                                BindingResult result,
                                Model model){
-        User existingUser = userService.findUserByEmail(userDto.getEmail());
-
+        User existingUser = UserService.findUserByEmail(userDto.getEmail());
         if(existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()){
             result.rejectValue("email", null,
                     "There is already an account registered with the same email");
@@ -38,7 +43,7 @@ public class AuthController {
             return "/register";
         }
 
-        userService.saveUser(userDto);
+        UserService.saveUser(userDto);
         return "redirect:/register?success";
     }
 }
