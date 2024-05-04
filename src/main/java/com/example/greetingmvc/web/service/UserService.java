f1299405cdc077240fabdd622e066f4a89ecd9b5
Cyrine <cyrine.karoui123@gmail.com>
2024-05-04 19:53:25 +0100

    registration done

--- src/main/java/com/example/greetingmvc/web/service/UserService.java
+++ src/main/java/com/example/greetingmvc/web/service/UserService.java
@@ -1,7 +1,10 @@
 package com.example.greetingmvc.web.service;
 
+import com.example.greetingmvc.web.models.Role;
 import com.example.greetingmvc.web.models.User;
 import com.example.greetingmvc.web.models.Dto.UserDto;
+import com.example.greetingmvc.web.repositories.RoleRepository;
+
 import java.util.List;
 
 
@@ -11,4 +14,6 @@ public interface UserService {
     User findUserByEmail(String email);
 
     List<UserDto> findAllUsers();
-}
+
+    }
+
