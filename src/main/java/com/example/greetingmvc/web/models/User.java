2c3cd174718e0663e567ec1692f9cc1e11215a99
eyahellal <eyahellal9@gmail.com>
2024-05-04 19:02:19 +0100

    userfixed

--- src/main/java/com/example/greetingmvc/web/models/User.java
+++ src/main/java/com/example/greetingmvc/web/models/User.java
@@ -16,6 +16,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable=false)
+    private String name;
+
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
