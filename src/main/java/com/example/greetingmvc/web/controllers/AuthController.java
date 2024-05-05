631d8540043d31bbf5848f4a67a8f1b06e0ec377
eyahellal <eyahellal9@gmail.com>
2024-05-05 17:40:45 +0100

    profile

--- src/main/java/com/example/greetingmvc/web/controllers/AuthController.java
+++ src/main/java/com/example/greetingmvc/web/controllers/AuthController.java
@@ -66,6 +66,7 @@ public class AuthController {
     public String login(){
         return "login";
     }
+    
 }
 
 
---
+++ src/main/java/com/example/greetingmvc/web/controllers/ProfileController.java
@@ -0,0 +1,13 @@
+package com.example.greetingmvc.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileController {
+
+    @GetMapping("/profile")
+    public String userProfile() {
+        return "profile"; // Return the name of your profile view/template
+    }
+}
