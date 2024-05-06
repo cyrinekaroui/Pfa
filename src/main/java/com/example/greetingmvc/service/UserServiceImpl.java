21a810e09b60ea51613825defb99f6e7c85b9337
eyahellal <eyahellal9@gmail.com>
2024-05-06 10:11:51 +0100

    herobiz deleted

--- src/main/java/com/example/greetingmvc/service/UserServiceImpl.java
+++ src/main/java/com/example/greetingmvc/service/UserServiceImpl.java
@@ -11,7 +11,6 @@ import com.example.greetingmvc.repositories.RoleRepository;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
